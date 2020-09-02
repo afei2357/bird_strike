c366ec5ba254abe029056468b8ea632a1ebc61bb
afei2357 <afei2357@qq.com>
2020-09-02 11:19:12 +0800

    注释celery部分的功能，打包后体积从680M缩小到54M

--- backend/app/rpt/tasks.py
+++ backend/app/rpt/tasks.py
@@ -2,18 +2,19 @@ import json
 import os
 import time
 from datetime import datetime
-import celery 
+# import celery 
 from app.extensions import db
 from app.rpt.reporter  import RunInfo
 from app.models1 import OrderMed
 
 
-@celery.task(bind=True)
-def generate_report(self,id):
+# @celery.task(bind=True)
+# def generate_report(self,id):
+def generate_report(id):
     from app import create_app
     app = create_app()
     with app.app_context():
-        self.update_state(state='RUNNING')
+        # self.update_state(state='RUNNING')
         order = OrderMed.query.get_or_404(id)
         client_snp = {i.rs:i.gt for i in order.updata.gtinfo.all()}
         client_info = (order.to_dict())
@@ -27,7 +28,7 @@ def generate_report(self,id):
         order.pdf_path = ins.pdf_path
         db.session.commit()
 
-@celery.task(bind=True)
+# @celery.task(bind=True)
 def add_together(self,x,y):
     #time.sleep(10) 
     for i in range(10):
