fb4d7f5c9c6412aefe70cc4deec2ec8ab9f0d485
afei <afei2357@qq.com>
2021-05-21 16:21:32 +0800

    小改

--- backend/run.py
+++ backend/run.py
@@ -6,5 +6,5 @@ app.debug = True
 if __name__ == '__main__':
     #print(app.config)
     db.create_all(app=app)
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=6000, debug=True)
     #app.run(host='0.0.0.0',port=6000, debug=True)
